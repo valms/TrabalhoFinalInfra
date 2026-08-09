059f5b23536f3e383952a3c7cc85f091c08b0b7a
Valmar Júnior <valmar.junior1@gmail.com>
2026-08-09 12:27:34 -0300

    chore: centraliza config do backend S3

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "unifor-terraform-state-trabalho-final"
     key          = "trabalho-final/terraform.tfstate"
-    region       = "sa-east-1"
     encrypt      = true
     use_lockfile = true
   }
